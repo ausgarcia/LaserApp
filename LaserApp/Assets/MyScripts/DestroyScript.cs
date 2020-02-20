3cf4ceda48d3701c351c00994b5cfdce3fabdf82
Austin Garcia <ausgarcia@outlook.com>
2020-02-20 13:44:50 -0600

    Removed github plugin, hitting lasers stops timer and spawning

--- LaserApp/Assets/MyScripts/DestroyScript.cs
+++ LaserApp/Assets/MyScripts/DestroyScript.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class DestroyScript : MonoBehaviour {
     int i = 0;
+    private GameObject manager;
+    private TimeManager TM;
+    private LaserSpawner LS;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
+
+        manager = GameObject.Find("Controller");
+        TM = manager.GetComponent<TimeManager>();
+        LS = manager.GetComponent<LaserSpawner>();
+
+    }
+	private void OnTriggerEnter(Collider other)
+    {
+        print("COLLISION");
+        if (other.gameObject.tag == "laser")
+        {
+            print("LASER");
+            TM.stopTime();
+            LS.stopSpawning();
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 		if(i == 1) //destroy gameobject after one frame
--- LaserApp/Assets/MyScripts/TimeManager.cs
+++ LaserApp/Assets/MyScripts/TimeManager.cs
@@ -10,6 +10,7 @@ public class TimeManager : MonoBehaviour {
     private List<GameObject> currentTouchColliders;
     private List<Touch> currentTouches;
     private int prevTouches;
+    private bool stopped;
     // Use this for initialization
     void Start () {
         timer = 0;
@@ -18,22 +19,22 @@ public class TimeManager : MonoBehaviour {
         multText.fontSize = (int)(Screen.width / 16.0f);
         multText.gameObject.GetComponent<RectTransform>().localPosition = new Vector3(0, -timerText.fontSize+20, 0);
         prevTouches = 0;
+        stopped = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         int multiplier = 0;
-        if (Input.touchCount > 0)
+        if (!stopped)
         {
-            //print("touched");
-            
-            multiplier = Input.touchCount;
-            timer += Time.deltaTime*multiplier;
-            //timer = System.Math.Truncate(1000 * timer) / 1000;
-            timerText.text = string.Format("{0:N2}",timer);
-            multText.text = "x" + multiplier;
-            //if (multiplier > prevTouches)
-            //{
+            if (Input.touchCount > 0)
+            {
+                //print("touched");
+
+                multiplier = Input.touchCount;
+                timer += Time.deltaTime * multiplier;
+                timerText.text = string.Format("{0:N2}", timer);
+                multText.text = "x" + multiplier;
                 for (int i = 0; i < Input.touchCount; i++)
                 {
                     Touch t = Input.GetTouch(i);
@@ -42,35 +43,18 @@ public class TimeManager : MonoBehaviour {
                     //Current Bug, size of collider changes due to screen size/orientation
                     GameObject finger = Instantiate(TouchColliderPrefab, new Vector3(Camera.main.ScreenToWorldPoint(t.position).x, Camera.main.ScreenToWorldPoint(t.position).y, (float)-4.5), new Quaternion(90, 0, 0, 0));
                     finger.transform.eulerAngles = new Vector3(90, 0, 0);
-                finger.AddComponent<DestroyScript>();// Destroys collider after one frame
-                    //Destroy(finger);
-                    //currentTouchColliders.Add(finger);//One of these only allows one finger to appear at a time
-                    //currentTouches.Add(t);
+                    finger.AddComponent<DestroyScript>();// Destroys collider after one frame
                 }
-            //}
-            /*else if (multiplier < prevTouches)
+            }
+            else if (!multText.text.Equals("x0"))
             {
-                GameObject colliderToDelete = currentTouchColliders[currentTouchColliders.Count - 1];
-                currentTouchColliders.Remove(colliderToDelete);
-                Destroy(colliderToDelete);
-
-                Touch touchToDelete = currentTouches[currentTouches.Count - 1];
-                currentTouches.Remove(touchToDelete);
-            }*/
-            /*for(int i = 0; i < currentTouchColliders.Count; i++)
-            {
-                currentTouchColliders[i].transform.position = new Vector3(Camera.main.ScreenToWorldPoint(currentTouches[i].position).x, Camera.main.ScreenToWorldPoint(currentTouches[i].position).y, (float)-4.5);
-
-            }*/
+                multText.text = "x0";
+            }
+            prevTouches = multiplier;
         }
-        else if(!multText.text.Equals("x0"))
-        {
-            multText.text = "x0";
-        }
-        prevTouches = multiplier;
-
-
-        //GameObject g = Instantiate(TouchColliderPrefab, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, (float)-4.5), Quaternion.identity);
-        //g.transform.eulerAngles = new Vector3(90, 0, 0);
 	}
+    public void stopTime()
+    {
+        stopped = true;
+    }
 }
