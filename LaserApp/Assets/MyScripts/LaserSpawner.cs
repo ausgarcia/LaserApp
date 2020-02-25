7f392bf0bab0db828c2626c75dee31f212928e9e
Austin Garcia <ausgarcia@outlook.com>
2020-02-25 11:56:22 -0600

    Lasers spawn at random angles

--- LaserApp/Assets/MyScripts/LaserSpawner.cs
+++ LaserApp/Assets/MyScripts/LaserSpawner.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LaserSpawner : MonoBehaviour {
     public GameObject laserPrefab;
+    private int numLasersToSpawn;
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("spawnLaser", 1, 2);
-        
+        numLasersToSpawn = 1;
     }
 	
 	// Update is called once per frame
@@ -16,7 +17,7 @@ public class LaserSpawner : MonoBehaviour {
 	}
     void spawnLaser()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < numLasersToSpawn; i++)
         {
             float spawnY = Random.Range
                 (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
@@ -24,7 +25,11 @@ public class LaserSpawner : MonoBehaviour {
                 (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
 
             Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+            float rand = Random.Range(0f, 180f);
+            //Quaternion laserQuat = new Quaternion(Quaternion.identity.x, Quaternion.identity.y, rand, Quaternion.identity.w);
+            print(rand);
             GameObject laser = Instantiate(laserPrefab, spawnPosition, Quaternion.identity);
+            laser.transform.eulerAngles  = new Vector3(laser.transform.eulerAngles.x, laser.transform.eulerAngles.y, rand);
             laser.AddComponent<LaserDestroyScript>();
             laser.GetComponent<LaserDestroyScript>().framesToWait = 100;
         }
