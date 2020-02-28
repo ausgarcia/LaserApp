c6a5d91fb2976de733712d7a9fcf87971384f4c3
Austin Garcia <ausgarcia@outlook.com>
2020-02-28 02:40:06 -0600

    Circle effect created from collision point with laser

--- LaserApp/Assets/MyScripts/DestroyScript.cs
+++ LaserApp/Assets/MyScripts/DestroyScript.cs
@@ -7,14 +7,20 @@ public class DestroyScript : MonoBehaviour {
     private GameObject manager;
     private TimeManager TM;
     private LaserSpawner LS;
+    private bool startCircleAnim;
+    private bool destroy;
+    private float circleSize;
+    private GameObject CircleEffect;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         manager = GameObject.Find("Controller");
         TM = manager.GetComponent<TimeManager>();
         LS = manager.GetComponent<LaserSpawner>();
+        startCircleAnim = false;
+        destroy = true;
+        circleSize = 0.01f;
 
     }
 	private void OnTriggerEnter(Collider other)
@@ -26,11 +32,25 @@ public class DestroyScript : MonoBehaviour {
             TM.stopTime();
             LS.stopSpawning();
             other.gameObject.GetComponent<LaserDestroyScript>().dontDestroy();
+            CircleEffect = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            
+            CircleEffect.transform.localScale= new Vector3(circleSize, circleSize, 1.0f);
+            CircleEffect.transform.position = this.transform.position;
+            startCircleAnim = true;
+            destroy = false;
+            this.GetComponent<MeshRenderer>().enabled = false;
         }
     }
 	// Update is called once per frame
 	void Update () {
-		if(i == 1) //destroy gameobject after one frame
+        if (startCircleAnim && circleSize < 400)
+        {
+            circleSize += .4f;
+            //CircleEffect
+            CircleEffect.transform.localScale = new Vector3(circleSize, circleSize, 1.0f);
+            CircleEffect.layer = 8;
+        }
+		if(i == 1 && destroy) //destroy gameobject after one frame
         {
             Destroy(this.gameObject);
         }
